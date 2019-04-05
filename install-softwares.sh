b2b58dd86433a2cfccf85676409fd41121501b3d
drpdishant <drpdishant@gmail.com>
2019-04-05 20:05:27 +0530

    Update install-softwares.sh

--- install-softwares.sh
+++ install-softwares.sh
@@ -1,1 +1,39 @@
 #!/bin/bash
+sudo apt-get update
+echo ">>     Download Chrome Package"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/Downloads
+echo ">>     Download Rocket Chat"
+wget https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/2.15.1/rocketchat_2.15.1_amd64.deb -P ~/Downloads
+
+
+echo ">>     Download and Install Sublime"
+
+echo ">> ------ Install the GPG key: "
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo ">> ------ Ensure apt is set up to work with https sources:"
+
+sudo apt-get install apt-transport-https
+echo ">> ------ Select the channel to use: Stable"
+
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo ">>     Upadte Cache & Install Sublime Text"
+sudo apt-get update
+sudo apt-get install sublime-text
+
+echo ">>     Install Docker CE"
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
