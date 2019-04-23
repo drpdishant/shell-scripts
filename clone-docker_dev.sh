a7d3fcab6a8d29827c658d7218f3c21bfddfef67
drpdishant <drpdishant@gmail.com>
2019-04-23 18:59:47 +0530

    Add git clone script

--- clone-docker_dev.sh
+++ clone-docker_dev.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+shopt -s expand_aliases
 
+alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
+
+echo ">>>>> Clone Drupal8-vagrant Repo"
+
+gituser=saurabhd
+
+
+read -p "Enter Git Password for ($gituser):" -s gitpass
+
+giturlpass=`urlencode $gitpass`
+
+dockerdev="https://$gituser:$giturlpass@github.com/saurabhd/Drupal8-vagrant.git"
+
+#git pull $dockerdev ~/Drupal8-vagrant/
+git clone $dockerdev
--- setup.sh
+++ setup.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+chrome="https://raw.githubusercontent.com/drpdishant/shell-scripts/master/install-chrome.sh"
+rocketchat="https://raw.githubusercontent.com/drpdishant/shell-scripts/master/install-rocketchat.sh"
+sublime="https://raw.githubusercontent.com/drpdishant/shell-scripts/master/install-sublime.sh"
+docker="https://raw.githubusercontent.com/drpdishant/shell-scripts/master/install-docker.sh"
+
+bash -c "$(curl -sL $chrome)"
+
+bash -c "$(curl -sL $rocketchat"
+
+bash -c "$(curl -sL $sublime)"
+
+bash -c "$(curl -sL $docker"
+
