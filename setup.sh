78946a8de85fd9692cf619da4e55851ccab3c0c8
drpdishant <drpdishant@gmail.com>
2019-04-24 12:40:24 +0530

    Prompt for Branch input

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-branch=dev
+read -p "Enter Branch (dev or master): " branch
+
 chrome="https://raw.githubusercontent.com/drpdishant/shell-scripts/$branch/install-chrome.sh"
 rocketchat="https://raw.githubusercontent.com/drpdishant/shell-scripts/$branch/install-rocketchat.sh"
 sublime="https://raw.githubusercontent.com/drpdishant/shell-scripts/$branch/install-sublime.sh"
 docker="https://raw.githubusercontent.com/drpdishant/shell-scripts/$branch/install-docker.sh"
+gitclone="https://raw.githubusercontent.com/drpdishant/shell-scripts/$branch/clone-docker_dev.sh"
 
 bash -c "$(curl -sL $chrome)"
 
-bash -c "$(curl -sL $rocketchat"
+bash -c "$(curl -sL $rocketchat)"
 
 bash -c "$(curl -sL $sublime)"
 
 bash -c "$(curl -sL $docker)"
+
+bash -c "$(curl -sL $gitclone)"
 
