32164a1b8f14b75fd3068112f8fe5627e2527734
Dishant Pandya <drpdishant@gmail.com>
2019-04-29 23:13:23 +0530

    Printing the Already installed version if found.

--- install-chrome.sh
+++ install-chrome.sh
@@ -4,11 +4,13 @@ echo ">>     CHeck for Chrome package and Download if not exists"
 if ls $HOME/Downloads/google-chrome* 1> /dev/null 2>&1; then
     echo "Chrome Package exists"
 else
-    echo "Chrome doesn't exist   >>>>> Downloading "
+    echo "Chrome doesn't exist   >>>>> Downloading"
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/Downloads
 fi
 
 if [ $(dpkg-query -W -f='${Status}' google-chrome-stable 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-sudo dpkg -i ~/Downloads/google-chrome*
+    sudo dpkg -i ~/Downloads/google-chrome* 
+else
+echo "$(google-chrome-stable --version) already installed"
 fi
