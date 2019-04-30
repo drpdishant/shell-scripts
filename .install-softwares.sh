693f698687c8c046b6b3a658d9c251d9413856fa
drpdishant <drpdishant@gmail.com>
2019-04-30 18:38:20 +0530

    making wget silent

--- .install-softwares.sh
+++ .install-softwares.sh
@@ -6,7 +6,7 @@ if ls $HOME/Downloads/google-chrome* 1> /dev/null 2>&1; then
     echo "Chrome Package exists"
 else
     echo "Chrome doesn't exist   >>>>> Downloading "
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/Downloads
+    wget -qO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/Downloads
 fi
 
 echo ">>     CHeck for Rocket chat package and Download if not exists"
@@ -15,7 +15,7 @@ if ls $HOME/Downloads/rocketchat* 1> /dev/null 2>&1; then
     echo "Rocket Chat Package exists"
 else
     echo "Rocket.Chat doesn't Exist  >>>>> Downloading "
-    wget https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/2.15.2/rocketchat_2.15.2_amd64.deb -P ~/Downloads
+    wget -qO https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/2.15.2/rocketchat_2.15.2_amd64.deb -P ~/Downloads
 fi
 
 if [ $(dpkg-query -W -f='${Status}' google-chrome-stable 2>/dev/null | grep -c "ok installed") -eq 0 ];
