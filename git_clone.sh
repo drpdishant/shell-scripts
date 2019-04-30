45ab5df20d56b181682d0a2ca29285b8e6d17892
drpdishant <drpdishant@gmail.com>
2019-04-30 15:52:51 +0530

    git clone

--- git_clone.sh
+++ git_clone.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-shopt -s expand_aliases
 
-alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
-alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
+#shopt -s direxpand
+#shopt -s expand_aliases
+
+#alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
+#alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
 
 echo ">>>>> Clone Drupal8-vagrant Repo"
 
@@ -13,7 +15,7 @@ read -p "Enter Git Password for ($gituser):" -s gitpas
 
 giturlpass=`urlencode $gitpass`
 
-dockerdev="https://$gituser:$giturlpass@github.com/saurabhd/Drupal8-vagrant.git"
+dockerdev="https://github.com/saurabhd/Drupal8-vagrant.git"
 
 
 if [ -d ~/Drupal8-vagrant ]
