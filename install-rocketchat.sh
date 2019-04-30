b5c0631685a5aed96b75fa2f37678ab6b31f289f
drpdishant <drpdishant@gmail.com>
2019-04-30 12:42:58 +0530

    Update install-rocketchat.sh

--- install-rocketchat.sh
+++ install-rocketchat.sh
@@ -5,8 +5,8 @@ if ls $HOME/Downloads/rocketchat* 1> /dev/null 2>&1; then
     echo -e "Rocket Chat Package exists\n"
 else
     echo -e "Rocket.Chat doesn't Exist  >>>>> Downloading \n"
-    wget "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/$latest/rocketchat_$latest_amd64.deb" -P ~/Downloads
-fi
+    wget "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/$latest/rocketchat_${latest}_amd64.deb" -P ~/Downloads
+fi{
 if [ $(dpkg-query -W -f='${Status}' rocketchat 2>/dev/null | grep -c "install ok installed") -eq 0 ];
 then
 sudo dpkg -i ~/Downloads/rocketchat*
