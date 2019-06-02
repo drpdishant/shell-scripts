1f05c6e4b5362deffd70738f940df47738b894e5
Dishant Pandya <dishant@localhost.localdomain>
2019-06-03 01:40:53 +0530

    resolve rocket-chat version checking - issue for installed package

--- install-rocketchat_fedora.sh
+++ install-rocketchat_fedora.sh
@@ -11,15 +11,16 @@ fi
 if [ $(dnf -q list installed rocketchat &>/dev/null && echo "1" || echo "0") -eq 0 ];
 then
 echo -e ">> Rocket chat is not installed - Installing...."
-sudo dnf install -y ~/Downloads/rocketchat-$latest.x86_64.rpm
+ sudo dnf install -y ~/Downloads/rocketchat-$latest.x86_64.rpm
 else
-echo -e "RocketChat $(dpkg -s rocketchat | grep '^Version:') Already Installed\n"
-    current=$(dpkg -s rocketchat | grep '^Version:' | awk '{print $2}' | cut -f1 -d "-")
+current=$(dnf -q list installed rocketchat | awk 'FNR == 2 {print $2}' | sed 's/-.*//')
+echo -e "RocketChat $current Already Installed\n"
 echo -e "Checking if it can be updated\n"
+
     if [ $current != $latest ]
     then
     echo -e "A newer version is available : $latest \n\n - Downloading & Installing "
-    rm -rf $HOME/Downloads/rocketchat*
+    rm -rf ~/Downloads/rocketchat*
     wget -q https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/$latest/rocketchat-$latest.x86_64.rpm -P ~/Downloads
     sudo dnf install -y -qq ~/Downloads/rocketchat-$latest.x86_64.rpm
     echo -e "Updated Rocketchat to $(dpkg -s rocketchat | grep '^Version:')\n"
