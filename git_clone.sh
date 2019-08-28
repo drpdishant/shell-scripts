211b17eda891f9532d1d408f92382699d31627a3
Noaman-addweb <50651327+Noaman-addweb@users.noreply.github.com>
2019-08-28 17:59:01 +0530

    Update git_clone.sh
    .

--- git_clone.sh
+++ git_clone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo ">>>>> Clone Drupal8-vagrant Repo"
 
-dockerdev="https://Noaman-addweb:addweb@github.com/Noaman-addweb/Drupal8-vagrant.git"
+dockerdev="https://Noaman-addweb:addweb123@github.com/Noaman-addweb/Drupal8-vagrant.git"
 
 
 if [ -d ~/Drupal8-vagrant ]
