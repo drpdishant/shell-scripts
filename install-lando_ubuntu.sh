d865b47e3f489bc386fd3f00b80758f765b3dc11
noaman-addweb <noaman.addweb@gmail.com>
2019-10-08 18:42:06 +0530

    added lando script

---
+++ install-lando_ubuntu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+function get_version {
+  IFS='/'
+  read -ra LINK <<< "$1"
+  for i in "${LINK[@]}"; do
+    TAG_NAME=$i
+  done
+  echo $TAG_NAME
+}
+
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run lando install script as root"
+  exit
+fi
+
+LAST_LINK=$(curl -I https://github.com/lando/lando/releases/latest | grep "Location" | tr -d '\r')
+LAST_LINK=${LAST_LINK:10}
+LAST_LINK="${LAST_LINK/tag/download}"
+VERSION=$(get_version $LAST_LINK)
+
+DOWNLOAD_LINK="${LAST_LINK}/lando-${VERSION}.deb"
+curl -sL -o/var/cache/apt/archives/lando-$VERSION.deb $DOWNLOAD_LINK
+dpkg -i /var/cache/apt/archives/lando-$VERSION.deb
+exit
