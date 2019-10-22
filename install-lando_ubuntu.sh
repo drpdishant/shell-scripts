116e13c1ff958298f2142f724b6ea7e45a586deb
Noaman-addweb <50651327+Noaman-addweb@users.noreply.github.com>
2019-10-22 15:18:00 +0530

    Update install-lando_ubuntu.sh

--- install-lando_ubuntu.sh
+++ install-lando_ubuntu.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
-function get_version {
-  IFS='/'
-  read -ra LINK <<< "$1"
-  for i in "${LINK[@]}"; do
-    TAG_NAME=$i
-  done
-  echo $TAG_NAME
-}
+latest=$(curl --silent "https://api.github.com/repos/lando/lando/releases" | grep -Po '"tag_name": "\K.*?(?=")' | head -n1)
+package_name="lando.deb"
 
+if ls $HOME/Downloads/lando* 1> /dev/null 2>&1; then
+    echo -e "lando Package exists\n"
+else
+    echo -e "lando doesn't Exist  >>>>> Downloading \n"
+    cd ~/Downloads && { curl -L -o $package_name https://github.com/lando/lando/releases/download/$latest/lando-$latest.deb; cd -; }
+fi
 
-#if [ "$EUID" -ne 0 ]
-#  then echo "Please run lando install script as root"
-#  exit
-#fi
-
-LAST_LINK=$(curl -I https://github.com/lando/lando/releases/latest | grep "Location" | tr -d '\r')
-LAST_LINK=${LAST_LINK:10}
-LAST_LINK="${LAST_LINK/tag/download}"
-VERSION=$(get_version $LAST_LINK)
-
-DOWNLOAD_LINK="${LAST_LINK}/lando-${VERSION}.deb"
-sudo curl -sL -o/var/cache/apt/archives/lando-$VERSION.deb $DOWNLOAD_LINK
-sudo dpkg -i /var/cache/apt/archives/lando-$VERSION.deb
-exit
+if [ $(dpkg-query -W -f='${Status}' lando 2>/dev/null | grep -c "install ok installed") -eq 0 ];
+then
+sudo dpkg -i ~/Downloads/lando*
+sudo apt-get -y -qq -f install  
+else
+echo -e "lando $(lando version) Already Installed\n"
+    current=$(lando version)
+echo -e "Checking if it can be updated\n"
+    if [ $current != $latest ]
+    then
+    rm -rf $HOME/Downloads/lando*
+    cd ~/Downloads && { curl -L -o $package_name https://github.com/lando/lando/releases/download/$latest/lando-$latest.deb; cd -; }
+    sudo dpkg -i ~/Downloads/lando*
+    sudo apt-get -y -qq -f install  
+    echo -e "Updated lando to $(lando version)\n"
+    else
+    echo -e "No Update Found!\n"
+    fi
+fi
