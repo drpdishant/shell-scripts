1ba2f35ef67d5e32cf7bb7edaaaf56b8b466601c
Dishant Pandya <drpdishant@gmail.com>
2020-04-03 09:38:59 +0530

    Delete upload-fp.sh

--- upload-fp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#find and delete any data older than a week.
-echo "Removing Week old Data"
-find . -type f -name "*.csv" -mtime +7 -exec rm {} \;
-
-#Sync data with the remote server
-echo "Syncing Data with Remote Server"
-rsync -r -e  "ssh  -o StrictHostKeyChecking=no -i "<path-to-key>"" . <user>@<host>:<remote-path> --delete
